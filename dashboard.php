9e3ba6796bca89afc82dfa80c70dab23c965a51c
Maneka123 <maneka611997@gmail.com>
2022-05-05 11:23:21 +0530

    created the dashboard page

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Welcome to Dashboard.</p>
+<p><a href="index.php">Home</a><p>
+<p><a href="insert.php">Insert New Item</a></p>
+<p><a href="view.php">View Item Records</a><p>
+<p><a href="logout.php">Logout</a></p>
+</div>
+</body>
+</html>

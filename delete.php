391437d7db1cf78856abfb7b5c9c4136e45a5167
Maneka123 <maneka611997@gmail.com>
2022-05-05 12:21:59 +0530

    created code to delete

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+//require('db.php');
+
+$servername='localhost';
+    $username='root';
+    $password='';
+    $dbname = "inventorymanagement";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+    
+      if(!$conn){
+          die('Could not Connect MySql Server:' .mysqli_error());
+        }
+
+
+$id=$_REQUEST['id'];
+$query = "DELETE FROM inventory WHERE itemCode=$id"; 
+$result = mysqli_query($conn,$query) or die ( mysqli_error());
+header("Location: view.php"); 
+?>

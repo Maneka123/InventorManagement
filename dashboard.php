801a34d7be8df92667931c03c89286e1606d9095
Maneka123 <maneka611997@gmail.com>
2022-05-06 07:56:01 +0530

    added search link to dashboard

--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,7 @@
 <p><a href="insert.php">Insert New Item</a></p>
 <p><a href="view.php">View Item Records</a><p>
 <p><a href="logout.php">Logout</a></p>
+<p><a href="mySearch.html">Search Item</a></p>
 </div>
 </body>
 </html>

8aaac5ad66995265f1d7e4f07129115a79a5869a
Maneka123 <maneka611997@gmail.com>
2022-05-06 11:49:15 +0530

    designed the graph page using css

--- graph.php
+++ graph.php
@@ -25,12 +25,58 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Graph</title> 
         </head>
+
+        <style>
+    .footer {
+      position : sticky;
+       left: 0;
+       bottom: 0;
+       width: 100%;
+       height:70px;
+       background-color: red;
+       color: white;
+       text-align: center;
+    }
+    </style>
+    
+    <style>
+    .header {
+      padding: 10px 16px;
+      background: #555;
+      color: #f1f1f1;
+      text-align: center;
+    
+    }
+    </style>
+<style>
+    body {
+      background-color: white;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }</style>
+<body>
+
         <body>
+
+        <div class="header" id="myHeader">
+        <h2>Inventory Management</h2>
+      </div>
+
+
+
             <div style="width:60%;hieght:20%;text-align:center">
                 <h2 class="page-header" >Analytics Reports </h2>
+                <h3>The following graph has the date in the x - axis and the number of items in the y - axis.</h3>
+                <h3>It shows number of items modified each day</h3>
                 <div>Product </div>
                 <canvas  id="chartjs_bar"></canvas> 
             </div>    
+
+            <div class="footer">
+        <p></p>
+      </div>
+
+
         </body>
       <script src="//code.jquery.com/jquery-1.9.1.js"></script>
       <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>

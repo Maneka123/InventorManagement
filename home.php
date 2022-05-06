6ed0d313f8ed34dda0afd5b127aaa1170ede8ddd
Maneka123 <maneka611997@gmail.com>
2022-05-06 17:44:16 +0530

    added additional details to homepage

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body >
+    <div align="center">
+        <img src="b6.png">
+<h1>Registration number : IT19980782</h1>
+<h1>Name : M. M. Wijesundara</h1>
+<h1>Module : IT Project Management</h1>
+<h1>Semester : Year 3- semester 1</h1>
+<h1>Specialization : Information Technology</h1>
+<h1>University : SLIIT</h1>
+<h1>Group : REG/WD/73</h1>
+</div>
+<p><a href="dashboard.php">Dashboard</a><p>
+</body>
+</html>
